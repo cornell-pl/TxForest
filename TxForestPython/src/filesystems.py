db0188e975fb04c6af03d007ffc6f9c77b549719
Katie Mancini <klm299@cornell.edu>
2019-11-19 16:50:24 -0500

    simple in mem fs (python)

--- TxForestPython/src/filesystems.py
+++ TxForestPython/src/filesystems.py
@@ -50,10 +50,48 @@ class MemoryFilesystem(Filesystem):
   def __getitem__(self, i):
     return self.fs[i]
 
+  def __contains__(self, i):
+    return i in self.fs
+
+  def _remove_dir(self, path):
+    us = self.fs[path].get_lst()
+    for u in us:
+      child_path = join(path, [u])
+      if isinstance(self.fs[child_path], FileContents):
+        del self.fs[child_path]
+      else:
+        self._remove_dir(child_path)
+    del self.fs[path]
+
+  def _simple_add(self, path, v):
+    if isinstance(v, FileContents):
+      self.fs[path] = v
+    else:
+      self.fs[path] = v
+      new_lst = v.get_lst()
+      for u in new_lst:
+        child_path = join(path, [u])
+        if not child_path in self.fs:
+          self.fs[child_path] FileContents('')
+
+
 
   def __setitem__(self, i, v):
-    #TODO: realistically this is more complicated but this is the idea we want
-    self.fs[i] = v
+    if i in self.fs:
+      if isinstance(v, FileContents) and isinstance(self.fs[i], DirContents):
+        self._remove_dir(i)
+      elif isinstance(v, DirContents) and isinstance(self.fs[i], DirContents):
+        new_lst = v.get_lst()
+        old_lst = self.fs[i].get_lst()
+        for u in old_lst:
+          if not u in new_lst:
+            child_path = join(path, [u])
+            if isinstance(self.fs[child_path], FileContents):
+              del self.fs[child_path]
+            else:
+              self._remove_dir(child_path)
+
+    self._simple_add(i, v)
 
 #TODO: realistically this is more complicated but this is the idea we want
 class PosixFilesystem(Filesystem):
