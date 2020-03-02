67dcfe068f1422eed7f29fab7e5fd799e9437607
Jonathan DiLorenzo <jonathan.dilorenzo@gmail.com>
2020-03-02 12:39:23 -0500

    Made TxForestPython more self-contained and fixed a few bugs

--- TxForestPython/tests/__main__.py
+++ TxForestPython/tests/__main__.py
@@ -1,10 +1,10 @@
 
 
 from test import *
-  path = '/Users/katie/Documents/examples/simple'
-  # test_spec0()
-  # test_spec1()
-  # test_spec2()
-  # test_spec3()
-  # test_spec4()
-  test_spec7(path)
+path = 'example_fs_root/simple'
+# test_spec0()
+# test_spec1()
+# test_spec2()
+# test_spec3()
+# test_spec4()
+test_spec7(path)
